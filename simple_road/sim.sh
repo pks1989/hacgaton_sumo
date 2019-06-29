9cc3b520db1d473954827c64155add4884f400d5
pks <pks1989@ya.ru>
2019-06-30 05:43:29 +0700

    set relative path

--- simple_road/sim.sh
+++ simple_road/sim.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # This is a comment!
-/var/www/sumo/bin/sumo -c simple_road.sumocfg --no-step-log --summary-output stdout
+sumo -c simple_road.sumocfg --no-step-log --summary-output stdout
